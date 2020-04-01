0260a8b2cba99099f78f056022cfd7f1de3be11e
ferdyjh <63008377+ferdyjh@users.noreply.github.com>
2020-04-01 23:06:27 +0700

    Add files via upload

---
+++ login2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="CodePixar">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Daftar</title>
+
+		<link rel="stylesheet" href="login2.css" />
+			
+		</head>
+		<body>
+		<form class="form">
+			<!-- Title -->
+			<h2 class="form-title">Sign up</h2>
+			<!-- Error -->
+			<div class="error-block">
+				<p class="error"></p>
+			</div>
+			<!-- Username -->
+			<div class="input-block">
+				<label for="username" class="label">Username</label>
+				<input type="text" name="username" class="input username-input" />
+			</div>
+			<!-- Email -->
+			<div class="input-block">
+				<label for="email" class="label">Email</label>
+				<input type="text" name="email" class="input email-input" />
+			</div>
+			<!-- Password -->
+			<div class="input-block">
+				<label for="password" class="label">Password</label>
+				<input type="password" name="password" class="input pw-input" />
+				<p class="input-info">Must be at least 6 characters long</p>
+			</div>
+			<!-- Repeat Password -->
+			<div class="input-block">
+				<label for="repeat_password" class="label">Repeat password</label>
+				<input type="password" name="repeat_password" class="input rpw-input" />
+			</div>
+			<!-- Button -->
+			<div class="input-block">
+				<input type="submit" value="Register" class="input-submit" />
+			</div>
+		</form>
+           
+		<script src="script1.js"></script>
+	</body>
+</html>
